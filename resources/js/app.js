1aea99ae05c6cf073d689948aa338086dfc8c4ba
Kelvin van der Gaag <kelvinvandergaag@protonmail.com>
2023-07-13 17:54:09 +0200

    Finalized converting wordpress to laravel with vite as asset builder

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,29 @@
 import './bootstrap';
+import '../scss/app.scss';
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+let menu = document.getElementById("hamburger-menu");
+let explainer = document.getElementById("course-item-explainer");
+let course_item = document.getElementsByClassName("course-items");
+let navigation = document.getElementById("navigation");
+
+
+menu.addEventListener("click", function (){
+    navigation.classList.toggle("show");
+});
+
+navigation.addEventListener("click", function (){
+    navigation.classList.toggle("show");
+});
+
+course_item.addEventListener("click", function (){
+    console.log("test");
+    explainer.style.height = "100";
+});
+
+
