ddc0bf373cadb778c67bdd39112c523015eff974
Kelvin van der Gaag <kelvinvandergaag@protonmail.com>
2023-07-13 20:03:20 +0200

    Created home button in navbar

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -12,7 +12,7 @@
 
 <header class="top-header">
     <div class="account-management">
-        <i class="fas fa-user"></i>
+        <a href="{{url('/')}}"><i class="fas fa-home"></i></a>
         <i class="fas fa-bell"></i>
     </div>
     <div class="hamburger-menu" id="hamburger-menu"><i class="fas fa-bars"></i></div>
