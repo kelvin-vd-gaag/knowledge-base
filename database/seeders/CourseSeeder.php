8b5ed8fd757264273d46f4f7301152835be138d7
Kelvin van der Gaag <kelvinvandergaag@protonmail.com>
2023-07-13 23:54:13 +0200

    Written homepage feature test

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -28,7 +28,7 @@ class CourseSeeder extends Seeder
             ]);
         //create 1 courses called javascript with lorem ipsum content
         \App\Models\Course::factory()->create([
-            'title' => 'Javascript',
+            'title' => 'JavaScript',
             'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
              Nullam euismod, nisl eget aliquam ultricies, nunc',
             'slug' => 'javascript',
---
+++ tests/Feature/HomepageTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomepageTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200)
+            ->assertSee('HTML')
+            ->assertSee('PHP')
+            ->assertSee('JavaScript');
+    }
+}
